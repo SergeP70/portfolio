303998c60ba72609d42c67ad1795f84230f27e38
sergeP70 <serge@pille.be>
2024-12-31 10:40:13 +0100

    display csv data into the columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import csv
 
 st.set_page_config(layout='wide')
 
@@ -15,3 +16,28 @@ with col2:
     Quisque et tellus cursus risus blandit vehicula faucibus sed erat. Duis non mauris massa. 
     Curabitur ornare erat eu lectus finibus, vitae suscipit quam fringilla."""
     st.info(content)
+
+content2= """ "Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+st.write(content2)
+
+cols = st.columns(2)
+cols_index = 0
+with open("data.csv", "r") as file:
+    csv_reader = csv.DictReader(file, delimiter=';')
+    for row in csv_reader:
+        with cols[cols_index]:
+            print(row['title'])
+            img_url = "images/" + row['image']
+            st.subheader(row['title'])
+            st.write(row['description'])
+            st.image(img_url, width=200)
+            st.write(row['url'])
+            if cols_index == 0:
+                cols_index = 1
+            else:
+                cols_index = 0
+
+
+
+
+
